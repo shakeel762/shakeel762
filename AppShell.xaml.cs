23b992a0e324872645a0764de491472a0b173438
shakeel762 <98880635+shakeel762@users.noreply.github.com>
2022-11-30 11:47:20 +0500

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiApp1.Pages;
+
+namespace MauiApp1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+        Routing.RegisterRoute(nameof(DetailPage), typeof(DetailPage));
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using MauiApp1.ViewModels;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MainViewModel>();
+
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddTransient<MainViewModel>();
+
+        return builder.Build();
+	}
+}
